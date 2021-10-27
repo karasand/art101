1e977e075001c8285f76addeed064b7e6acf1654
karasand <91683903+karasand@users.noreply.github.com>
2021-10-27 10:15:38 -0700

    Lab 8 :)

---
+++ lab8/js/lab.js
@@ -0,0 +1,29 @@
+ /*
+  * Author: Kayla Sanders, karasand@ucsc.edu>
+  * Created: 10/27/2021
+  * Lisence: public domain
+  */
+
+
+
+function myFunction(x){
+    return(x % 2 = 0);
+}
+
+//test function
+console.log("Is 1 even?", myFunction(1));
+console.log("Is 2 even?", myFunction(2));
+
+array = [2, 56, 8, 75, 37, 325, 953]
+console.log("My array", array);
+
+var result = array.map(myFunction);
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x){
+
+})
+
+console.log("Squareroot of array:", result);
+
+//output
